8d8aabdc89c134f7b64be38d3b1c77109f67b52b
nazarethjim <nazarethjim@gmail.com>
2016-03-07 11:29:00 +0100

    Add new params

--- repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/users/migrateuser.post.json.ftl
+++ repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/users/migrateuser.post.json.ftl
@@ -1,1 +1,1 @@
-{ "test": ${fromJava} }
+{ "code": ${code} }
--- repo-amp/src/main/java/com/users/MigrateUser.java
+++ repo-amp/src/main/java/com/users/MigrateUser.java
@@ -19,9 +19,14 @@ package com.users;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.security.PersonService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.DeclarativeWebScript;
 import org.springframework.extensions.webscripts.Status;
+import org.springframework.extensions.webscripts.WebScriptException;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 
 /**
@@ -30,11 +35,39 @@ import org.springframework.extensions.webscripts.WebScriptRequest;
  * @author nazareth.jimenez@ixxus.com
  */
 public class MigrateUser extends DeclarativeWebScript {
+
+    private static Log logger = LogFactory.getLog(MigrateUser.class);
+
+    private PersonService personService;
+
     @Override
-    protected Map<String, Object> executeImpl(
-            final WebScriptRequest req, final Status status, final Cache cache) {
+    protected Map<String, Object> executeImpl(final WebScriptRequest req, final Status status, final Cache cache) {
         final Map<String, Object> model = new HashMap<String, Object>();
-        model.put("fromJava", "HelloFromJava");
+        model.put("code", 0);
+
+        final String newuser = req.getParameter("newuser");
+        final String olduser = req.getParameter("olduser");
+        final String sites = req.getParameter("sites");
+        final String groups = req.getParameter("groups");
+        final String content = req.getParameter("content");
+        final String comments = req.getParameter("comments");
+        final String userhome = req.getParameter("userhome");
+        final String datauser = req.getParameter("datauser");
+        final String likes = req.getParameter("likes");
+        final String favorites = req.getParameter("favorites");
+        final String workflows = req.getParameter("workflows");
+
+        final NodeRef newuserRef;
+        NodeRef olduserRef;
+
+        if((newuser == null) || (olduser == null)){
+            throw new WebScriptException("Missing mandatory params");
+        }
+
+        if (personService.personExists(olduser)) {
+            olduserRef = personService.getPerson(olduser);
+        }
+
         return model;
     }
 }
