ea6762daf87eea09ccd228cfd2f0b9e23859f29d
Abarrachina <agarciabarrachina@gmail.com>
2016-03-15 14:48:17 +0100

    Enabling set of workflow initiator

---
+++ repo-amp/src/main/java/com/users/migrateservice/dao/ActivitiProcessDAO.java
@@ -0,0 +1,8 @@
+package com.users.migrateservice.dao;
+
+import com.users.migrateservice.ProcessStarterUser;
+
+public interface ActivitiProcessDAO {
+
+	public int executeUpdateAuthor (ProcessStarterUser input);
+}
