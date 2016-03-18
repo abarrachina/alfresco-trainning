b698ca0cac2f4c3d847b0f8fabe8ec0f77247f1f
Abarrachina <agarciabarrachina@gmail.com>
2016-03-18 10:29:12 +0100

    Improving test coverage

--- repo-amp/src/test/java/com/ixxus/ipm/migration/users/MigrateServiceUserHomeTest.java
+++ repo-amp/src/test/java/com/ixxus/ipm/migration/users/MigrateServiceUserHomeTest.java
@@ -2,8 +2,10 @@ package com.ixxus.ipm.migration.users;
 
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -15,6 +17,7 @@ import javax.inject.Inject;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.policy.BehaviourFilter;
+import org.alfresco.service.cmr.lock.NodeLockedException;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -88,16 +91,37 @@ public class MigrateServiceUserHomeTest {
         childsOldHome.add(child1);
 
         when(nodeService.getChildAssocs(nodeRefOldHome)).thenReturn(childsOldHome);
-        when(nodeService.getChildByName(eq(nodeRefNewHome), eq(ContentModel.ASSOC_CONTAINS), any(String.class))).thenReturn(null);
-        when(nodeService.moveNode(nodeRefChild1OldUser, nodeRefNewHome,  ContentModel.ASSOC_CONTAINS, ContentModel.ASSOC_CHILDREN)).thenReturn(child2);
+        when(nodeService.getChildByName(eq(nodeRefNewHome), eq(ContentModel.ASSOC_CONTAINS), any(String.class))).thenReturn(null);        
         when(nodeService.getType(any(NodeRef.class))).thenReturn(ContentModel.TYPE_CONTENT);
     }
 
     @Test
     public void testMigrateHome() {
+    	when(nodeService.moveNode(nodeRefChild1OldUser, nodeRefNewHome,  ContentModel.ASSOC_CONTAINS, ContentModel.ASSOC_CHILDREN)).thenReturn(child2);    	
     	migrateServiceUserHome.migrate(olduser, newuser);
         verify(nodeService, times(1)).setProperty(child2.getChildRef(), ContentModel.PROP_CREATOR, newuser);
         verify(nodeService, times(1)).setProperty(child2.getChildRef(), ContentModel.PROP_MODIFIER, newuser);
+    }
+    
+    @Test
+    public void testNodeLockedException(){
+    	int notMigrated = migrateServiceUserHome.getNotMigrate().size();
+    	doThrow(new NodeLockedException()).when(nodeService).moveNode(nodeRefChild1OldUser, nodeRefNewHome, ContentModel.ASSOC_CONTAINS, ContentModel.ASSOC_CHILDREN);
+    	
+    	migrateServiceUserHome.migrate(olduser, newuser);
+    	//Checking 2 new nodes migrated have failed
+    	assertTrue(migrateServiceUserHome.getNotMigrate().size() == notMigrated + 1);
+    }
+    
+    @Test
+    public void testUnexistingChildNode(){
+    	int notMigrated = migrateServiceUserHome.getNotMigrate().size();
+    	NodeRef dummyNodeRef = new NodeRef("workspace://SpacesStore/newnoderefchild1");
+    	when(nodeService.getChildByName(eq(nodeRefNewHome), eq(ContentModel.ASSOC_CONTAINS), any(String.class))).thenReturn(dummyNodeRef);
+    	
+    	migrateServiceUserHome.migrate(olduser, newuser);
+    	//Checking 2 new nodes migrated have failed
+    	assertTrue(migrateServiceUserHome.getNotMigrate().size() == notMigrated + 1);
     }
 
 }
